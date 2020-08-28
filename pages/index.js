bb42727bb67c56970d95e20f375848ce9866c864
technicalrutvik <rutvikghori25@gmail.com>
2020-08-28 21:31:04 +0530

    done

--- pages/index.js
+++ pages/index.js
@@ -26,17 +26,7 @@ const Home = ({products}) => {
   )
 }
 
-// export async function getStaticProps(){
-//   const res = await fetch(`${baseUrl}/api/products`)
-//   const data =await res.json()
-//   return {
-//     props:{
-//       products:data
-//     }
-//   }
-// }
-
-export async function getServerSideProps(){
+export async function getStaticProps(){
   const res = await fetch(`${baseUrl}/api/products`)
   const data =await res.json()
   return {
@@ -45,4 +35,14 @@ export async function getServerSideProps(){
     }
   }
 }
+
+// export async function getServerSideProps(){
+//   const res = await fetch(`${baseUrl}/api/products`)
+//   const data =await res.json()
+//   return {
+//     props:{
+//       products:data
+//     }
+//   }
+// }
 export default Home;
