1e8798cb99d6a082517bad45551a07ef894d1e4f
technicalrutvik <rutvikghori25@gmail.com>
2020-08-29 08:22:32 +0530

    done

--- pages/create.js
+++ pages/create.js
@@ -112,7 +112,7 @@ const Create = (props) => {
 export async function getServerSideProps(ctx){
   const cookie =   parseCookies(ctx)
   const user = cookie.user ? JSON.parse(cookie.user) : ""
-  if(user.role != 'admin'){
+  if(user.role ==='user' ||user.role ==='' ){
       const {res} = ctx
       res.writeHead(302,{Location:"/" })
       res.end()
